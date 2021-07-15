93bc8b3e5eeb137c8e3e35ae4e023d83d4d4153e
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-07-14 20:29:19 -0700

    added .env info for db

--- server/index.js
+++ server/index.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const port = 8000;
 
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
 
-const client = new Client({
-  user: 'mattwrobel',
-  host: 'localhost',
-  database: 'mydb',
-  password: '',
-  port: 5432,
-})
-client.connect()
+const pool = new Pool(); // works like this using dotenv and .env file w/ db env variable!
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
+pool.connect();
+
+pool.query('SELECT * FROM product WHERE id = 1', (err, res) => {
+    console.log(err, res)
+    pool.end()
+  });
+
+// CAN ALSO USE CLIENT INSTEAD OF POOL -- EXPLORE THE DIFFERENCE
+
+// const client = new Client({
+//   user: 'mattwrobel',
+//   host: 'localhost',
+//   database: 'mydb',
+//   password: '',
+//   port: 5432,
+// })
+
+// client.connect();
+
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
 
 app.get('/', (req, res) => {
   res.status(200).send('Led Zeppelin')
