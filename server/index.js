db11fa053aaca281a1fdebebadb3ea3e2f7ee52d
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-07-14 18:53:53 -0700

    initialized postgres

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = 8000;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'mattwrobel',
+  host: 'localhost',
+  database: 'mydb',
+  password: '',
+  port: 5432,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
 
 app.get('/', (req, res) => {
   res.status(200).send('Led Zeppelin')
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`)
-})
+});
