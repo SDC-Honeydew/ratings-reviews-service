ea441dbe4953a98431478e8fc30c041aab618f1c
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-07-19 14:54:21 -0700

    changed test

--- server/models.js
+++ server/models.js
@@ -1,6 +1,7 @@
 const db = require('./db.js');
 
 module.exports = {
+
   get: (req) => {
 
     return new Promise ((resolve, reject) => {
@@ -8,7 +9,8 @@ module.exports = {
       db.Review.findAll({
         where: {
           product_id: 5
-        }
+        },
+        order: [['helpfulness', 'DESC']]
       })
         .then((data) => {
           resolve(data);
@@ -17,6 +19,7 @@ module.exports = {
           reject(err);
         })
     })
+
     //   return new Promise ((resolve, reject) => {
     //     db.query(`SELECT * FROM reviews WHERE product_id = ${req.query.product_id}`, (err, data) => {
     //       if (err) {
