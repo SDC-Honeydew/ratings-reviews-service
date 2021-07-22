1542a2c0858f71e3f7b26460428c16b5d1dfb49e
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-07-22 10:19:53 -0700

    refactored files for testing

--- server/index.js
+++ server/index.js
@@ -1,9 +1,5 @@
-const express = require('express');
-const app = express();
+const app = require('./server.js')
 const port = 8000;
-const router = require('./router.js');
-
-app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`)
