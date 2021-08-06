7320ea2c7d08e945e289bd5c6b7982c9dd16ad27
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-08-06 11:08:27 -0700

    refactored getMeta route

--- server/controllers.js
+++ server/controllers.js
@@ -1,4 +1,3 @@
-// const models = require('./models.js');
 const { models } = require('../sequelize');
 
 exports.get = async (req, res) => {
@@ -9,7 +8,6 @@ exports.get = async (req, res) => {
   const sortOrder = sort === 'newest' ? 'date' : 'helpfulness';
 
   try {
-    // const reviews = await models.getReviews(page, count, sort, product_id);
     const reviews = await models.review.findAll({
       attributes: [['id', 'review_id'], 'rating', 'summary', 'recommend', 'response', 'body', ['date_time', 'date'], 'reviewer_name', 'helpfulness'],
       limit: count,
@@ -36,72 +34,74 @@ exports.get = async (req, res) => {
     console.log(err);
     res.status(500).send(err);
   }
-
-
-
-  // return models.getReviews(page, count, sort, product_id)
-  //   .then((reviews) => {
-  //     let response = {
-  //       product: product_id.toString(),
-  //       page: Number(page),
-  //       count: reviews.length,
-  //       results: reviews
-  //     }
-  //     res.status(200).send(response);
-  //   })
-  //   .catch((err) => {
-  //     // console.log(err);
-  //     res.status(500).send(err);
-  //   })
 };
 
-exports.getMeta = (req, res) => {
+exports.getMeta = async (req, res) => {
   const { product_id } = req.query;
-  return Promise.all([models.getReviewsMeta(product_id), models.getCharacteristicsMeta(product_id)])
-    .then((results) => {
-      let reviewsMeta = results[0];
-      let characteristicsMeta = results[1];
-      // format the ratings and recommended objs
-      let ratings = {};
-      let recommend = {};
-      reviewsMeta.forEach((review) => {
-        if (!ratings[review.rating]) {
-          ratings[review.rating] = 1;
-        } else {
-          ratings[review.rating]++;
-        }
-        if (!recommend[review.recommend]) {
-          recommend[review.recommend] = 1;
-        } else {
-          recommend[review.recommend]++;
-        }
-      });
-      // format the characteristicsMeta obj -- naive implementation MAKE THIS A HELPER FUNCTION
-      let formattedCharacteristics = {};
-      characteristicsMeta.forEach((characteristic) => {
-        let value = 0;
-        characteristic.characteristic_reviews.forEach((review) => {
-          value += review.value;
-        })
-        value = value / characteristic.characteristic_reviews.length;
 
-        formattedCharacteristics[characteristic.name] = {
-          id: characteristic.id,
-          value: value
-        };
-      });
-      let response = {
-        product_id: product_id,
-        ratings: ratings,
-        recommended: recommend,
-        characteristics: formattedCharacteristics
+  try {
+    const results = await Promise.all([
+      models.review.findAll({
+        attributes: ['rating', 'recommend'],
+        where: {
+          product_id: product_id
+        }
+      }),
+      models.characteristic.findAll({
+        attributes: ['id', 'name'],
+        where: {
+          product_id: product_id
+        },
+        include: {
+          model: models.characteristic_review,
+          attributes: ['value']
+        },
+        order: [['id', 'ASC']]
+      })
+    ]);
+
+    let reviewsMeta = results[0];
+    let characteristicsMeta = results[1];
+    // format the ratings and recommended objs
+    let ratings = {};
+    let recommend = {};
+    reviewsMeta.forEach((review) => {
+      if (!ratings[review.rating]) {
+        ratings[review.rating] = 1;
+      } else {
+        ratings[review.rating]++;
       }
-      res.status(200).send(response);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    })
-  };
+      if (!recommend[review.recommend]) {
+        recommend[review.recommend] = 1;
+      } else {
+        recommend[review.recommend]++;
+      }
+    });
+    // format the characteristicsMeta obj -- naive implementation MAKE THIS A HELPER FUNCTION
+    let formattedCharacteristics = {};
+    characteristicsMeta.forEach((characteristic) => {
+      let value = 0;
+      characteristic.characteristic_reviews.forEach((review) => {
+        value += review.value;
+      })
+      value = value / characteristic.characteristic_reviews.length;
+
+      formattedCharacteristics[characteristic.name] = {
+        id: characteristic.id,
+        value: value
+      };
+    });
+    let response = {
+      product_id: product_id,
+      ratings: ratings,
+      recommended: recommend,
+      characteristics: formattedCharacteristics
+    };
+    res.status(200).send(response);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
 
 exports.postReview = ((req, res) => {
   // console.log('post request body: ', req.body);
