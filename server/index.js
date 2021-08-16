3dc2d7b7b7f809ca093ac54c4964f611d13905f9
Matthew Wrobel <matthew.wrobel@gmail.com>
2021-08-15 20:41:24 -0700

    added ping route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const router = require('./router.js');
 app.use(express.json());
+app.get('/ping', (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: 'OK',
+    date: new Date()
+  };
+  res.status(200).send(data);
+});
 app.use('/', router);
 
 module.exports = app;
